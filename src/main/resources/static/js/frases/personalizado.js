c7970b641b3b35468df894a884bd35fe26f315ab
Juan Serrano <jserranovalle@gmail.com>
2020-02-08 20:55:13 -0600

    para debug

--- src/main/resources/static/js/frases/personalizado.js
+++ src/main/resources/static/js/frases/personalizado.js
@@ -50,9 +50,9 @@ var frasesJs = (function() {
 			$("#btnsPracticar").hide();
 		},
 		keyPressEscritura : function() {
-			var frase = $("#f_0").text().trim().toLowerCase();
+			var frase = $("#f_0").text().toLowerCase();
 			alert(frase);
-			var escritura = $("#escritura").val().trim().toLowerCase();
+			var escritura = $("#escritura").val().toLowerCase();
 
 			if (escritura == frase) {
 				$("#escritura").addClass("is-valid");
