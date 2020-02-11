96ac8dd83c765d550c209a29d31cf9cd194d55f5
Juan Serrano <jserranovalle@gmail.com>
2020-02-11 09:24:13 -0600

    class="overflow-auto"

---
+++ src/main/java/com/jcsvall/app/utils/JavaUtil.java
@@ -0,0 +1,11 @@
+package com.jcsvall.app.utils;
+
+import java.util.List;
+
+import com.jcsvall.app.entities.Categorias;
+
+public class JavaUtil {
+	public static List<Categorias> obtenerCategorias(Integer idUsuario) {
+		return null;
+	}
+}
